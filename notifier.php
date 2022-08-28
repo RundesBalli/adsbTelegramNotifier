fdbbdbdcf77916817ee3d108491281ebd7b82019
RundesBalli <GitHub@RundesBalli.com>
2022-08-28 23:04:03 +0200

    Added notifier basic script

---
+++ notifier.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * adsbTelegramNotifier
+ * 
+ * Notifies via Telegram when an aircraft passes over a certain area.
+ * 
+ * @author    RundesBalli <GitHub@RundesBalli.com>
+ * @copyright 2022 RundesBalli
+ * @see       https://github.com/RundesBalli/adsbTelegramNotifier
+ */
+
+/**
+ * Including the configuration and function loader.
+ */
+require_once(__DIR__.DIRECTORY_SEPARATOR."includes".DIRECTORY_SEPARATOR."loader.php");
+
+/**
+ * This script always runs in verbose mode. Therefore its output has to be discarded when using cron.
+ */
+
