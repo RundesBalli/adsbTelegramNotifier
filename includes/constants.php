3cbeb314875b020475131765a383daba4f9c53f4
RundesBalli <GitHub@RundesBalli.com>
2022-08-28 23:05:32 +0200

    Added constants

---
+++ includes/constants.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * includes/constants.php
+ */
+
+/**
+ * Minimum config version
+ */
+const MIN_CONFIG_VERSION = 1;
+
+/**
+ * Clear screen
+ */
+const CLEAR_SCREEN = "\e[2J\e[1;1H";
+
+/**
+ * Emojis
+ */
+const EMOJI_AIRCRAFT = "\xF0\x9F\x9B\xA9";
+const EMOJI_INFO = "\xE2\x84\xB9";
+const EMOJI_WARN = "\xE2\x9A\xA0";
+
+/**
+ * Colors (Terminal)
+ */
+const COLOR_WARN = "\e[38;5;9m";
+const COLOR_OPENING = "\e[38;5;6m";
+const COLOR_FILE = "\e[38;5;6m";
+const COLOR_INFO = "\e[38;5;3m";
+const COLOR_OK = "\e[38;5;2m";
+const COLOR_RESET = "\e[0m";
+?>
