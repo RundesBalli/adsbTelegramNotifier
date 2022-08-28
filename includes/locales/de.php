399e33a017b4fe6b671ab243efe9826c4e680045
RundesBalli <GitHub@RundesBalli.com>
2022-08-28 23:07:58 +0200

    Added some text to German locale

--- includes/locales/de.php
+++ includes/locales/de.php
@@ -17,4 +17,11 @@ $lang['checkVersion']['error'] = "Es konnte nicht geprüft werden, ob es eine Ak
 $lang['checkVersion']['updateAvailable'] = "Es steht ein Update zur Verfügung!\n\nNavigiere im Terminal in das Verzeichnis des Notifiers und führe `git pull` aus um ein Update durchzuführen.";
 $lang['checkVersion']['versionOk'] = "Version aktuell.";
 
+/**
+ * notifier.php
+ */
+$lang['notifier']['checkConfigVersion'] = "Überprüfe Konfigurationsversion.";
+$lang['notifier']['updateConfig'] = "Die Konfigurationsdatei muss aktualisiert werden. Verwende die aktuelle Konfigurationsdatei aus dem Repository als Grundlage.";
+$lang['notifier']['checkConfigVersionDone'] = "Konfigurationsversion OK.";
+$lang['notifier']['checkUpdateAvailable'] = "Prüfung auf Aktualisierungen.";
 ?>
